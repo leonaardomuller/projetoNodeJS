caadc9203ca1ab24acd1a92ea1475b483ff855e3
Leonardo Muller <leonaardo.muller@gmail.com>
2020-12-06 21:54:30 -0300

    Final

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns'
+import { parseISO } from 'date-fns'
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository'
+import CreateAppointmentService from '../services/CreateAppointmentService'
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository
@@ -12,11 +13,19 @@ appointmentsRouter.get('/', (request, response) => {
 })
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = parseISO(date);
-
-  return response.json(appointment)
+    const parsedDate = parseISO(date);
+  
+    const createAppointment = new CreateAppointmentService(appointmentsRepository);
+  
+    const appointment = createAppointment.execute({date: parsedDate, provider})
+  
+    return response.json(appointment)
+  } catch (err) {
+    return response.status(400).json({error: err.message})
+  }
 })
 
 export default appointmentsRouter;
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,36 +1,31 @@
+import { startOfHour } from 'date-fns'
 import Appointment from '../models/Appointment'
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
-/**
- * [x] Recebimento das informações
- * [/] Tratativa de erros/excessões
- * [] Acesso ao repositório
- */
 interface Request {
   provider: string;
   date: Date;
 }
 
-/**
- * Dependency Inversion
- */
-
 class CreateAppointmentService {
-  constructor() {
+private appointmentsRepository: AppointmentsRepository
 
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
   }
 
   public execute({date, provider}: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(
-      parsedDate,
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
     )
 
     if (findAppointmentInSameDate) {
       throw Error('This appointment is already booked')
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = this.appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
